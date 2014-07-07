7d25d13714254e901fa8b4629ddb1ba155125a91
Darius Lau <darius@disforma.com>
2014-07-07 09:53:21 -0500

    adds semantic ui for front end development

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Invox::Application.routes.draw do
 
-  get 'home' => 'static_pages'
-
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

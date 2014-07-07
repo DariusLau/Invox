e4e8ad5fe557f41ff1ee3e9900c7e41359771bc2
Darius Lau <darius@disforma.com>
2014-07-06 23:05:59 -0500

    Initiates invox app development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Invox::Application.config.secret_key_base = '05e3927acd094794289c0678279ee6bed99ba0c8ded7eb095890c5e18565bdfc0d2eaf7ccb8d84c91f948ad5e4721a0ec1cb08a3352f86c48c305b8799f6acc9'
